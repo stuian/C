2e13278ce240f09e06289fca63a0849a201a99e1
stuian <892545949@qq.com>
2018-03-14 17:10:49 +0800

    180314-notes

---
+++ code/S1E14-jiujiu-multi.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+int main()
+{
+	// ¥Ú”°æ≈æ≈≥À∑®±Ì
+	int i,j;int count;
+	for (i = 1;i < 10;i++) 
+	{
+		for (j = 1;j <= i;j++)
+		{
+			count = i * j;
+			printf("%d*%d = %d\n",i,j,count);
+		}
+	}
+}
--- code/S1E2-plane.c
+++ code/S1E2-plane.c
@@ -2,7 +2,7 @@
 	
 	int main()
 	{
-	        printf("\n\
+		printf("\n\
 	         @\n\
 	        / \\\n\
 	        * *\n\
