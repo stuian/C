73b946b6b4d3cc9b43778fe5544449498d154767
stuian <892545949@qq.com>
2018-03-18 21:41:04 +0800

    modified

--- code/test.c
+++ code/test.c
@@ -2,20 +2,8 @@
 
 int main()
 {
-        int i;
-
-        printf("请输入成绩：");
-        scanf("%d", &i);
-
-        switch (i)
-        {
-                case i >= 90: printf("A\n"); break;
-                case i >= 80 && i < 90: printf("B\n"); break;
-                case i >= 70 && i < 80: printf("C\n"); break;
-                case i >= 60 && i < 70: printf("D\n"); break;
-                case i < 60: printf("E\n"); break;
-                default: printf("请输入合法的数字！\n"); break;
-        }
+        while (1)
+                ;
 
         return 0;
 }
