9743afad4449fe07243bb1b7e65ff9ac11842a4d
stuian <892545949@qq.com>
2018-03-23 22:25:55 +0800

    test modified

--- code/test.c
+++ code/test.c
@@ -2,11 +2,19 @@
 
 int main()
 {
-        float num;
-        int status;
-        
-		scanf("%f", &num);
-		printf("%f",num);
+ 	int status ;
+ 	int sum = 0;
+ 	int num = 0;
+	printf("请输入合法的数字：");
+    do
+    {
+        status = scanf("%d", &num);
+        printf("%d\n",num);
+        sum = sum + num;
+        printf("%d\n",sum);
+    } while (getchar() != '\n' && status == 1);
+    printf("%d\n",sum);
 
-        return 0;
+
+    return 0;
 }
