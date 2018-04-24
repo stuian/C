f01a79c80c37ad3d1ed5f7fd77accaca455a36af
stuian <892545949@qq.com>
2018-04-24 23:47:55 +0800

    mid test

---
+++ code/S1Emid-test2.c
@@ -0,0 +1,55 @@
+# include <stdio.h> 
+# include <math.h>
+
+int main()
+{
+	// 600851475143
+	
+	int num	= 600851475143;
+	int i = 2;
+	int max = 1;
+	_Bool flag = 1; 
+	
+	half = (int)sqrt(num);
+	for (i;i<=half;i++)
+	{
+		if (num % i == 0)
+		{
+			if (i>max)
+			{
+				half-2 = (int)sqrt(i);
+				for (i;i<=half;i++)
+				{
+					if (num % i == 0)
+					{
+						;
+					}
+					else
+					{
+						;
+					}
+				}	
+				max = i;	
+			}
+		}
+	}
+	
+	while (flag)
+	{
+		factor = num / max; 
+	}	
+	
+	//判断是否是质数 
+	half = (int)sqrt(num);
+	for (i;i<=half;i++)
+	{
+		if (num % i == 0)
+		{
+			;
+		}
+		else
+		{
+			;
+		}
+	}
+}
---
+++ code/S1Emid-test3.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+
+int main()
+{
+	//两个三位数相乘得出的最大的回文数 
+	
+	//求倒置数 
+	num = 123;
+	while(i>0)
+	{
+		i = num % 10;
+		inversion = 
+		i = (num - i) % 
+		
+	}
+	
+}
